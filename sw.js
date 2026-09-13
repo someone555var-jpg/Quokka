b76f49fce33bf894cbee43f964a75bf6d3896247
someone555var-jpg <someone555var@gmail.com>
2026-09-13 18:41:26 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+const C = 'quokka-v2';
+self.addEventListener('install', e => e.waitUntil(caches.open(C).then(c => c.addAll(['./index.html']))));
+self.addEventListener('fetch', e => e.respondWith(
+  caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+    const copy = res.clone();
+    caches.open(C).then(c => c.put(e.request, copy));
+    return res;
+  }).catch(() => caches.match('./index.html')))
+);
